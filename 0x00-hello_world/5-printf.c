1a32be5fdb592f491dadb778f7ef00eec47634a2
Teddab <ttitet7@gmail.com>
2022-12-11 10:19:21 -0800

    add printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper programmer, but the outcome is a piece of art,\n");
+	return (0);
+}
