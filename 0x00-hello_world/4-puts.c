a73b58855827202b6c9829aabc9950ad94e52a3a
Teddab <ttitet7@gmail.com>
2022-12-11 09:34:25 -0800

    arranged coding of c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
 
-int main (void)
+int main(void)
 {
-	puts("\"programming like building multilingual puzzle");
-	return(0);
+	puts("\"programming like building a multilingual puzzle");
+	return (0);
 }
