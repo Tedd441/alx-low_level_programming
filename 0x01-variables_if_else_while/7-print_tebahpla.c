079163fc7426d7a3ac7d737d60e6fe41d01f86eb
Teddab <ttitet7@gmail.com>
2022-12-12 11:28:14 -0800

    omg

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/**
+ *
+ *  * main - printing a-z
+ *
+ *   *
+ *
+ *    * Return:0(sucess)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int i;
+
+
+
+			for (i = 'z'; i >= 'a'; i--)
+
+					{
+
+								putchar(i);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+}
