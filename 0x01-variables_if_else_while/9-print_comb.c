f73efc26adfc8ee33a54360a8ab309f32770a5aa
Teddab <ttitet7@gmail.com>
2022-12-12 11:34:04 -0800

    adda 9print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+/**
+ *
+ *  * main - printing a-z
+ *
+ *   *
+ *
+ *    * Return:0(sucess)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int i;
+
+
+
+			for (i = 48; i <= 57; i++)
+
+					{
+
+								putchar(i);
+
+										if (i == 57)
+
+													{
+
+																	putchar('\n');
+
+																			} else
+
+																						{
+
+																										putchar(',');
+
+																													putchar(' ');
+
+																															}
+
+																				}
+
+				return (0);
+
+}
