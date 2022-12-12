c967490e5fd9ab1c2f23b1bae8fe221d43fc54fa
Teddab <ttitet7@gmail.com>
2022-12-12 10:33:53 -0800

    add 3-alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - printing a-z
+ * 
+ * Return:0(success)
+ */
+int main(void)
+ {
+	 int i;
+
+	 for (i = 'a' ; i<='z' ; i++ )
+
+	 {
+		putchar(i);
+	 }
+
+	 for (i = 'A' ; i<='Z' ; i++)
+
+	 {
+		putchar(i);
+	 }
+	 	putchar('\n');
+
+		return (0);
+ }
