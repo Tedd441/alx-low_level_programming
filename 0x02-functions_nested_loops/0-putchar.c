da043717359fba6b81d8db4e2bfa55a9e5101086
Teddab <ttitet7@gmail.com>
2022-12-13 23:42:05 -0800

    1st putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Retur:0 (success)
+ */
+int main(void)
+{
+	char s[]="_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
